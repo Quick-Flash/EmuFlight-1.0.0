ff619351fbadf982b177274ca98721f41cd33284
Quick-Flash <46289813+Quick-Flash@users.noreply.github.com>
2020-05-31 23:27:39 -0600

    properly filter dterm

--- src/main/flight/rpm_filter.h
+++ src/main/flight/rpm_filter.h
@@ -40,7 +40,6 @@ PG_DECLARE(rpmFilterConfig_t, rpmFilterConfig);
 
 void  rpmFilterInit(const rpmFilterConfig_t *config);
 float rpmFilterGyro(int axis, float values);
-float rpmFilterDterm(int axis, float values);
 void  rpmFilterUpdate();
 bool isRpmFilterEnabled(void);
 float rpmMinMotorFrequency();
